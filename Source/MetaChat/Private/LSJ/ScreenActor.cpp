2d2d0e678b289a1d34fb770f4d36703c0f88b80f
Jun0123 <76517458+Jun0123@users.noreply.github.com>
2024-09-07 16:26:51 +0900

    화면 UI 수정 : 공유 화면 버튼 및 검정배경

--- Source/MetaChat/Private/LSJ/ScreenActor.cpp
+++ Source/MetaChat/Private/LSJ/ScreenActor.cpp
@@ -213,7 +213,7 @@ void AScreenActor::BeginPlay()
 	MainWidget = Cast<ULSJMainWidget>(CreateWidget<UUserWidget>(GetWorld(),MainWidgetFactory));
 	if (MainWidget)
 	{
-		MainWidget->AddToViewport(2);
+		MainWidget->AddToViewport(-1);
 		MainWidget->SetScreenActor(this);
 	}
 }
